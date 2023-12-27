7c5c5cd844f6015ebe52fb6bfde97294f5d1662f
hgiang12 <21120237@student.hcmus.edu.vn>
2023-12-27 23:10:22 +0700

    u12 - get list (valid) vouchers

--- orders/views.py
+++ orders/views.py
@@ -12,9 +12,26 @@ class VoucherViewSet(viewsets.ModelViewSet):
     queryset = Voucher.objects.all()
     serializer_class = VoucherSerializer
 
+    # For buyer: Get list of valid vouchers, sort by end date (ASC)
+    def get_queryset(self):
+        only_valid = self.request.query_params.get('only_valid', None)
+        current_datetime = timezone.now()
+
+        if only_valid == 'true':
+            return Voucher.objects.all().filter(end__gte=current_datetime,used_quantity__lt=F('quantity')).order_by('end')  
+
+
+    # For admin: Get list of all vouchers, sort by end date (DESC)    
+    def list(self, request):
+        vouchers = self.queryset.order_by('-end')
+        serializer = self.serializer_class(vouchers, many=True)
+        return Response(serializer.data)
+
+
+# Get the total number of valid vouchers
 @api_view(["GET"])
 def count_valid_vouchers(request):
     current_datetime = timezone.now()
     valid_vouchers_count = Voucher.objects.filter(end__gte=current_datetime,used_quantity__lt=F('quantity')).count()
-    
+
     return Response({"valid_vouchers_count": valid_vouchers_count}, status=200)
