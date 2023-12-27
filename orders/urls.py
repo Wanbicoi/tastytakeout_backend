dce44fc8191a89461b9bb8fbb022934c6ab043b3
bubunana <21120237@student.hcmus.edu.vn>
2023-12-27 22:36:34 +0700

    u12 - preview total of valid vouchers

--- orders/urls.py
+++ orders/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+from .views import VoucherViewSet
+from .views import count_valid_vouchers
+
+router = DefaultRouter()
+router.register(r"vouchers", VoucherViewSet)
+
+urlpatterns = [
+    path('vouchers/count-valid-vouchers/', count_valid_vouchers, name='count_valid_vouchers'),
+]
+
+urlpatterns += router.urls
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Voucher
+from .serializers import VoucherSerializer
+from rest_framework.decorators import api_view
+from drf_yasg.utils import swagger_auto_schema
+from django.utils import timezone
+from django.db.models import F
 
-# Create your views here.
+class VoucherViewSet(viewsets.ModelViewSet):
+    queryset = Voucher.objects.all()
+    serializer_class = VoucherSerializer
+
+@api_view(["GET"])
+def count_valid_vouchers(request):
+    current_datetime = timezone.now()
+    valid_vouchers_count = Voucher.objects.filter(end__gte=current_datetime,used_quantity__lt=F('quantity')).count()
+    
+    return Response({"valid_vouchers_count": valid_vouchers_count}, status=200)
