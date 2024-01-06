64329b798dfa5461c4f16ce7cd91bc55c0a36834
Pi ne <wanbicoi123@gmail.com>
2024-01-06 10:14:39 +0700

    fix permissions

--- utils/permissions.py
+++ utils/permissions.py
@@ -3,9 +3,9 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsSeller(BasePermission):
     def has_permission(self, request, view):  # type: ignore
-        return bool(request.method in SAFE_METHODS) and request.user.role == "SELLER"
+        return bool(request.method in SAFE_METHODS) or request.user.role == "SELLER"
 
 
 class IsBuyer(BasePermission):
     def has_permission(self, request, view):  # type: ignore
-        return bool(request.method in SAFE_METHODS) and request.user.role == "BUYER"
+        return bool(request.method in SAFE_METHODS) or request.user.role == "BUYER"
