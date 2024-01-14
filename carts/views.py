506df033dfa1b8fe8a8345ce6dbeed2d6a5759d0
Pi ne <wanbicoi123@gmail.com>
2024-01-14 18:26:37 +0700

    style

--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import get_object_or_404
-from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, viewsets
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from carts.models import Cart
 from carts.serializers import CartSerializer, GetCartSerializer
 
